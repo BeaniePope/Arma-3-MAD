bd28cb4c6e67ea9dd9e6b469137da137a0fa0d8e
BeaniePope <Swifter11110@gmail.com>
2024-02-20 22:47:32 -0500

    Folder changes

---
+++ SW MAD Auxiliary Mod/mod.cpp
@@ -0,0 +1,12 @@
+name		 = "Star Wars MAD Auxiliary Mod";								// Name of your mod
+picture 	 = ".paa"; 	// Picture displayed from the expansions menu. Optimal size is 2048x1024
+logoSmall	 = ".paa";	// Display next to the item added by the mod
+logo		 = "MADLogo.paa";			// Logo displayed in the main menu
+logoOver	 = ".paa";		// When the mouse is over, in the main menu
+actionName 	 = "Discord";
+action		 = "https://discord.gg/YWQBV6Bcab";			    // Website URL, that can accessed from the expansions menu
+tooltipOwned = "MAD Aux Mod";
+
+overview = "Mod created by Queen. Textures by Wulfsige";
+hideName	= 0;	// Hide the extension name
+hidePicture	= 0;	// Hide the extension menu
