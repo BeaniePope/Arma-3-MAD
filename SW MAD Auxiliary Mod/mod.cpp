ba84943186e1fe33624080cf259c554597f1bcdf
BeaniePope <Swifter11110@gmail.com>
2024-02-20 22:48:08 -0500

    logo

--- SW MAD Auxiliary Mod/mod.cpp
+++ SW MAD Auxiliary Mod/mod.cpp
@@ -1,7 +1,7 @@
 name		 = "Star Wars MAD Auxiliary Mod";								// Name of your mod
 picture 	 = ".paa"; 	// Picture displayed from the expansions menu. Optimal size is 2048x1024
 logoSmall	 = ".paa";	// Display next to the item added by the mod
-logo		 = "MADLogo.paa";			// Logo displayed in the main menu
+logo		 = "data\logo\MADLogo.paa";			// Logo displayed in the main menu
 logoOver	 = ".paa";		// When the mouse is over, in the main menu
 actionName 	 = "Discord";
 action		 = "https://discord.gg/YWQBV6Bcab";			    // Website URL, that can accessed from the expansions menu
